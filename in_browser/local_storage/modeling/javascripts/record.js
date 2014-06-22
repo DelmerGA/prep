b5c3b9c50996540ac28f3d993d3f6690101ebe39
Delmer Reed <delmer814@yahoo.com>
2014-06-21 22:24:27 -0700

    spike

---
+++ in_browser/local_storage/modeling/javascripts/record.js
@@ -0,0 +1,93 @@
+/*
+* Recommendation: use a collection of Records
+* that manages collections of all Records, but used 
+* by all other objects.
+*/
+
+function Record(){}
+
+
+Record.recordKeys = function(){
+  var name = this.name, keys;
+  keys = localStorage[name+"Keys"];
+  return keys? JSON.parse(localStorage[name + "Keys"]): null;
+};
+
+Record.makeKey = function(){
+  var keys = this.recordKeys(), name = this.name;
+
+  if(keys) {
+    console.log("WWhat???", keys)
+    var length = keys.length || 1, last = length - 1;
+    console.log(last,keys.length)
+    newKey = keys[last] + 1;
+  } else {
+    newKey = 1;
+    keys = [newKey];
+    localStorage[name+"Keys"] = JSON.stringify(keys);
+  }
+  
+  console.log("The key is: ", newKey);
+  while(localStorage[name+newKey] !== undefined){
+    newKey += 1;
+  }
+
+  return newKey;
+};
+
+Record.addObject = function(newObject){
+  var name = this.name, keys, newKey;
+
+  newKey = this.makeKey();
+  console.log("nameKeys", localStorage[name + "Keys"]);
+  keys = this.recordKeys();
+
+  keys.push(newKey);
+  localStorage[name+newKey] = JSON.stringify(newObject);
+  newObject.getKey = function(){return newKey};
+  
+  return newObject;
+};
+
+Record.all = function(){
+  var name, records, keys;
+  
+  records = [];
+  name = this.name,
+  keys = this.recordKeys();
+
+  if(keys){
+    for(var index = 0, key, record; index < keys.length; index++){
+      key = keys[index]
+      record = localStorage[name+key];
+      record.getKey = function(){return key;}
+      records.push(record);
+    }
+  } 
+  return records;
+}
+
+
+
+Record.create = function(params){
+  var name = this.name, newKey,
+    newRecord;
+
+  newRecord = new this(params);
+  return this.addObject(newRecord);
+};
+
+Record.prototype.save = function(){
+  var name = this.constructor.name, key;
+
+  if(!!this.getKey){
+    key = this.getKey();
+    localStorage[name+key] = JSON.stringify(this);
+  } else {
+    this.constructor.addObject(this);
+  }
+
+  return this;
+};
+
+
